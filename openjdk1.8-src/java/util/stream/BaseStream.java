f8c0df44e8816605892a726069434459eecdc6df
gaozhilai <gaozhilai@thunisoft.com>
2020-04-17 18:33:55 +0800

    java.util.stream.BaseStream接口添加注释

--- openjdk1.8-src/java/util/stream/BaseStream.java
+++ openjdk1.8-src/java/util/stream/BaseStream.java
@@ -60,9 +60,9 @@ import java.util.function.Predicate;
  * @see LongStream
  * @see DoubleStream
  * @see <a href="package-summary.html">java.util.stream</a>
- */
-public interface BaseStream<T, S extends BaseStream<T, S>>
-        extends AutoCloseable {
+ */ // 由 GaoZhilai 进行分析注释, 不正确的地方敬请斧正, 希望帮助大家节省阅读源代码的时间 2020/4/17 17:58
+public interface BaseStream<T, S extends BaseStream<T, S>> // 流的基本方法接口定义, 泛型T是流中包含元素类型, 泛型S是当前实际流类型
+        extends AutoCloseable { // 流支持try-with-resources语法糖
     /**
      * Returns an iterator for the elements of this stream.
      *
@@ -70,7 +70,7 @@ public interface BaseStream<T, S extends BaseStream<T, S>>
      * operation</a>.
      *
      * @return the element iterator for this stream
-     */
+     */ // 返回一个包含流中元素的迭代器
     Iterator<T> iterator();
 
     /**
@@ -80,7 +80,7 @@ public interface BaseStream<T, S extends BaseStream<T, S>>
      * operation</a>.
      *
      * @return the element spliterator for this stream
-     */
+     */ // 返回一个包含流中元素的可分割迭代器
     Spliterator<T> spliterator();
 
     /**
@@ -89,7 +89,7 @@ public interface BaseStream<T, S extends BaseStream<T, S>>
      * terminal stream operation method may yield unpredictable results.
      *
      * @return {@code true} if this stream would execute in parallel if executed
-     */
+     */ // 返回当前流执行流操作时是否是并发操作. 注意在调用流的终结方法之后再调用此方法会产生不可预知的结果
     boolean isParallel();
 
     /**
@@ -101,7 +101,7 @@ public interface BaseStream<T, S extends BaseStream<T, S>>
      * operation</a>.
      *
      * @return a sequential stream
-     */
+     */ // 返回一个与当前流等价的有序的流, 如果流本身有序那么返回本身, 否则将流处理成有序的返回
     S sequential();
 
     /**
@@ -113,7 +113,7 @@ public interface BaseStream<T, S extends BaseStream<T, S>>
      * operation</a>.
      *
      * @return a parallel stream
-     */
+     */ // 返回一个并发流, 当前流已经有序返回其自身, 否则将流底层修改为并发流并返回
     S parallel();
 
     /**
@@ -126,7 +126,7 @@ public interface BaseStream<T, S extends BaseStream<T, S>>
      * operation</a>.
      *
      * @return an unordered stream
-     */
+     */ // 返回一个无序流, 当前流已经无序返回自身, 否则流底层修改为无序的并返回
     S unordered();
 
     /**
@@ -146,7 +146,7 @@ public interface BaseStream<T, S extends BaseStream<T, S>>
      *
      * @param closeHandler A task to execute when the stream is closed
      * @return a stream with a handler that is run if the stream is closed
-     */
+     */ // 返回一个与当前流等价的, 添加了流关闭处理器的流. 当流调用close方法时, 流上存在的关闭处理器将按顺序执行
     S onClose(Runnable closeHandler);
 
     /**
@@ -154,7 +154,7 @@ public interface BaseStream<T, S extends BaseStream<T, S>>
      * to be called.
      *
      * @see AutoCloseable#close()
-     */
+     */ // 关闭当前流, 调用此方法会导致流上存在的所有关闭处理器被调用
     @Override
     void close();
 }
